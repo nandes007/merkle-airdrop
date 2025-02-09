69b4a7ec7457bb5c0ee3e36db6d10e63f0852a5a
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2025-02-09 11:52:46 +0700

    add SplitSignature script and format code styling

--- src/BagelToken.sol
+++ src/BagelToken.sol
@@ -5,7 +5,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BegelToken is ERC20, Ownable {
-    constructor() ERC20("Bagel", "BAGEL") Ownable(msg.sender) { }
+    constructor() ERC20("Bagel", "BAGEL") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -5,44 +5,45 @@ import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeE
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleAirdrop {
-   using SafeERC20 for IERC20;
-   error MerkleAirdrop__InvalidProof();
-   error MerkleAirdrop__AlreadyClaimed();
+    using SafeERC20 for IERC20;
 
-   // some list of address
-   // Allow someone in the list to claim tokens
-   address[] claimers;
-   bytes32 private immutable i_merkleRoot;
-   IERC20 private immutable i_airdropToken;
-   mapping(address claimer => bool claimed) private s_hasClaimed;
+    error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
 
-   event Claim(address account, uint256 amount);
+    // some list of address
+    // Allow someone in the list to claim tokens
+    address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+    mapping(address claimer => bool claimed) private s_hasClaimed;
 
-   constructor(bytes32 merkleRoot, IERC20 airdropToken) {
-      i_merkleRoot = merkleRoot;
-      i_airdropToken = airdropToken;
-   }
+    event Claim(address account, uint256 amount);
 
-   function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
-      if (s_hasClaimed[account]) {
-         revert MerkleAirdrop__AlreadyClaimed();
-      }
-      // calculate using the account and the amount, the hash -> leaf node
-      bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
-      if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
-         revert MerkleAirdrop__InvalidProof();
-      }
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
 
-      s_hasClaimed[account] = true;
-      emit Claim(account, amount);
-      i_airdropToken.safeTransfer(account, amount);
-   }
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        if (s_hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+        // calculate using the account and the amount, the hash -> leaf node
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
 
-   function getMerkleRoot() external view returns (bytes32) {
-      return i_merkleRoot;
-   }
+        s_hasClaimed[account] = true;
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
 
-   function getAirdropToken() external view returns (IERC20) {
-      return i_airdropToken;
-   }
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
 }
